e5d93e84e9081d09543a490f1325da6b7361eedb
Prateek09102001 <Prateek09102001@gmail.com>
2023-09-25 21:45:03 +0530

    Project upload

---
+++ lib/age_widget.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class AgeWidget extends StatefulWidget {
+
+  final Function(int) onChange;
+
+  final String title;
+
+  final int intiValue;
+
+  final int min;
+
+  final int max;
+
+  const AgeWidget({super.key,
+    required this.onChange,
+    required this.title,
+  required this.intiValue,
+    required this.min,
+    required this.max});
+
+  @override
+  State<AgeWidget> createState() => _AgeWidgetState();
+}
+
+class _AgeWidgetState extends State<AgeWidget> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    counter = widget.intiValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+    child: Card(
+    elevation: 12,
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20)),
+    child: Column(
+    children: [
+      Text(widget.title,
+      style: const TextStyle(fontSize: 20,color: Colors.grey),),
+      const SizedBox(height: 10,),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+
+          children: [
+            InkWell(
+            child: const CircleAvatar(
+              radius: 12,
+              backgroundColor: Colors.blue,
+              child: Icon(
+                Icons.remove,
+                color: Colors.white,
+              ),
+            ),
+            onTap: (){
+              setState(() {
+                if(counter > widget.min){
+                  counter--;
+                }
+              });
+              widget.onChange(counter);
+            },),
+
+            const SizedBox(width: 15,),
+            Text(counter.toString(),
+              textAlign: TextAlign.center,
+            style: const TextStyle(color: Colors.black87,
+                fontSize: 18,fontWeight: FontWeight.w500
+            ),),
+            const SizedBox(width: 15,),
+            InkWell(
+              child: const CircleAvatar(
+                radius: 12,
+                backgroundColor: Colors.blue,
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+              onTap: (){
+                setState(() {
+                  if(counter < widget.max){
+                    counter++;
+                  }
+                });
+                widget.onChange(counter);
+              },),
+          ],
+        ),
+      )
+    ]),
+    ),
+    );
+  }
+}
---
+++ lib/height_widget.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HeightWidget extends StatefulWidget {
+
+  final Function(int) onChange;
+
+  const HeightWidget({super.key,required this.onChange});
+
+  @override
+  State<HeightWidget> createState() => _HeightWidgetState();
+}
+
+class _HeightWidgetState extends State<HeightWidget> {
+
+  int _height = 150;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Card(
+          elevation: 12,
+          shape: const RoundedRectangleBorder(),
+      child: Column(
+        children: [
+         const Text('Height',
+            style: TextStyle(fontSize: 25, color: Colors.grey),
+          ),
+          const SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(_height.toString(),style: const TextStyle(fontSize: 40),),
+              const SizedBox(width: 10,),
+              Text("Cm",style: TextStyle(fontSize: 25, color: Colors.black), )
+            ],
+          ),
+          Slider(
+            min: 0,
+              max: 240,
+            autofocus: false,
+            value: _height.toDouble(),
+            thumbColor: Colors.red,
+            onChanged: (value){
+              setState(() {
+                _height = value.toInt();
+              });
+              widget.onChange(_height);
+            },
+          )
+        ],
+      )),
+    );
+  }
+}
